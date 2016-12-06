ceab547e86b92baefd5306af158ba9c14d7102f1
Ray <1064435394@qq.com>
2016-12-06 17:23:13 +0800

    第一个版本

---
+++ Content/js/addLoadEvent.js
@@ -0,0 +1,12 @@
+﻿function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.load = func;
+    } else {
+        window.onload = function () {
+            oldonload();
+           
+        }
+    }
+    func();
+}
---
+++ Content/js/prepareInternalnav.js
@@ -0,0 +1,43 @@
+﻿function prepareInternalnav() {
+    if (!document.getElementById) return false;
+    if (!document.getElementsByTagName) return false;
+    if (!document.getElementsByClassName) return false;
+    var foot = document.getElementsByTagName("footer");
+    if (foot.length == 0) return false;
+    var link = foot[0].getElementsByClassName("link");
+    if (link.length == 0) return false;
+    var links = link[0].getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        var image = links[i].getElementsByTagName("img");
+        if (image.length == 0) return false;
+        var img = image[0].getAttribute("src").split("/")[3];
+        var sectionId = "";
+        if (i == 0) {
+            sectionId = img.split(".")[0].split("e")[0];
+        } else {
+            sectionId = img.split(".")[0];
+        }
+
+
+        if (!document.getElementById(sectionId)) continue;
+        if (sectionId != "chat") {
+            document.getElementById(sectionId).style.display = "none";
+        }
+
+        links[i].destination = sectionId;
+
+       
+        //var onImg = image[0].getAttribute("src").split(".png")[0] + "ed.png";
+        links[i].onclick = function () {
+            var hash = window.location.hash.split("#")[1];
+            if (hash != this.destination || hash == "") {
+                window.location.href = "#" + this.destination;
+            }
+
+            selectNav(this.destination);
+            showSection(this.destination);
+            return false;
+        }
+
+    }
+}
